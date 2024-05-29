d2426c314031836f15f411c83ce9042ede12e99e
Umang Bhardwaj <umangbhardwaj.ji@gmail.com>
2024-05-29 16:07:12 +0530

    add connection

---
+++ server/config/db.js
@@ -0,0 +1,23 @@
+var mongoose = require("mongoose");
+require("dotenv").config();
+
+const collectionName = "IdeaCollection";
+
+const ideaCollectionSchema = new mongoose.Schema({
+  id: { type: Number, unique: true },
+  ideaName: { type: String },
+  description: { type: String },
+  category: { type: String },
+  ownerName: { type: String },
+  ownerId: { type: Number },
+  reusable: { type: String },
+  businessValue: { type: String },
+  comments: { type: String },
+  link: { type: String },
+  pictureFile: { type: String },
+  createdDate: { type: Date },
+  ownerEmail: { type: String },
+  ideaId: { type: Number, unique: true },
+});
+var IdeaCollection = mongoose.model(collectionName, ideaCollectionSchema);
+module.exports = IdeaCollection;
