d6aa518db01b9c8473e2f360ae9ad2d1628b66da
Umang Bhardwaj <umangbhardwaj.ji@gmail.com>
2024-05-29 12:20:36 +0530

    validate Bearer token

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,24 +1,25 @@
 const jwt = require("jsonwebtoken");
-
 const JWT_SECRET = "your_jwt_secret_key"; // Make sure this matches in both files
 
 const authenticateJWT = (req, res, next) => {
   const authHeader = req.headers.authorization;
-
-  if (authHeader) {
-    const token = authHeader.split(" ")[1];
-
-    jwt.verify(token, JWT_SECRET, (err, user) => {
-      if (err) {
-        return res.sendStatus(403);
-      }
-
-      req.user = user;
-      next();
-    });
-  } else {
-    res.sendStatus(401);
+  if (!authHeader) {
+    return res.status(403).send({ message: "No token provided" });
   }
+
+  const parts = authHeader.split(" ");
+  if (parts.length !== 2 || parts[0] !== "Bearer") {
+    return res.status(403).send({ message: "Token format is invalid" });
+  }
+
+  const token = parts[1];
+  jwt.verify(token, JWT_SECRET, (err, user) => {
+    if (err) {
+      return res.status(500).send({ message: "Failed to authenticate token" });
+    }
+    req.user = user;
+    next();
+  });
 };
 
 module.exports = authenticateJWT;
