84effdd63fc1fcbc4597c03713cb9329d3fc15e0
Umang Bhardwaj <umangbhardwaj.ji@gmail.com>
2024-05-29 21:50:21 +0530

    add idea router

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 // server.js
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
+const ideaRoutes = require("./routes/idea");
 const cors = require("cors");
 
 const app = express();
@@ -17,11 +19,23 @@ app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
 
+const url = process.env.DATABASE_URL;
+mongoose
+  .connect(url)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB", err);
+  });
+
+app.use(express.json());
 app.use(bodyParser.json());
 app.use("/api/auth", authRoutes);
 
 // Protected routes
 app.use("/api", userRoutes);
+app.use("/api/idea", ideaRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
