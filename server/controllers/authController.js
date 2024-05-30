9e7b430a2ee334cf9a0b2869172f0cea2e6dc3ef
Umang Bhardwaj <umangbhardwaj.ji@gmail.com>
2024-05-30 09:42:56 +0530

    modify users DB

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,27 +1,38 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const { users } = require("../models/userModel");
+const User = require("../models/userModel");
 
 const JWT_SECRET = "your_jwt_secret_key"; // Make sure this matches in both files
 
 const register = async (req, res) => {
   const { username, password } = req.body;
+  try {
+    const userExists = await User.findOne({ username });
+    console.log(userExists);
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
 
-  const userExists = users.find((user) => user.username === username);
-  if (userExists) {
-    return res.status(400).json({ message: "User already exists" });
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new User({
+      username,
+      password: hashedPassword,
+    });
+    await newUser.save();
+
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {
+    console.error("Error during registration:", error); // Log the error for debugging
+    res.status(500).json({ message: "Server error" });
   }
-
-  const hashedPassword = await bcrypt.hash(password, 10);
-
-  users.push({ username, password: hashedPassword });
-  res.status(201).json({ message: "User registered successfully" });
 };
 
 const login = async (req, res) => {
   const { username, password } = req.body;
   try {
-    const user = users.find((user) => user.username === username);
+    const user = await User.findOne({ username });
+    // const user = users.find((user) => user.username === username);
     if (!user) {
       return res.status(401).json({ message: "Invalid credentials" });
     }
@@ -32,7 +43,7 @@ const login = async (req, res) => {
     }
 
     const token = jwt.sign({ username: user.username }, JWT_SECRET, {
-      expiresIn: "86400",
+      expiresIn: "1h",
     });
     // res.json({ token });
     res.status(200).send({
